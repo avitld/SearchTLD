aa057424ce8677925ca4f2510c278df8634df0cd
Avitld <avitld@terrorist.lol>
2023-06-13 22:43:52 +0300

    Fix proxy image bug

--- main/proxy-image.php
+++ main/proxy-image.php
@@ -1,5 +1,5 @@
 <?php
-	$url = htmlspecialchars($_REQUEST["url"],ENT_QUOTES,'UTF-8');
+	$url = $_REQUEST["url"];
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36');
