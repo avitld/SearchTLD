97b18c03a58e42f578625db799b1d0951c7944dc
Avitld <avitld@terrorist.lol>
2023-07-05 21:48:23 +0300

    Small UI tweak for visual pleasure

--- main/index.php
+++ main/index.php
@@ -2,15 +2,24 @@
 		<title>Home - SearchTLD</title>
 	</head>
 	<?php require "other/background.php"; ?>
+	<?php require "other/placeholder.php"; ?>
 	<body>
 		<div class="indexform">
 			<h1>Search<span id="purple">TLD</span></h1>
 			<form method="get" autocomplete="off" action="search.php">
-				<input type="search" name="q" autofocus required placeholder="What will you search for today?" value="<?php echo htmlspecialchars($_GET['q'] ?? '', ENT_QUOTES); ?>">
+				<div class="search-container">
+					<input type="search" name="q" autofocus required placeholder="<?php $numb = pickRand(); returnArray($numb, $searchPlaceholders); ?> " value="<?php echo htmlspecialchars($_GET['q'] ?? '', ENT_QUOTES); ?>"><button type="submit">
+					<?php
+						$theme = isset($_COOKIE["theme"]) ? $_COOKIE["theme"] : 'dark';
+						if ($theme == 'light') {
+							echo "<img src=\"/static/img/mag_light.png\" />";
+						} else {
+							echo "<img src=\"/static/img/mag_dark.png\" />";
+						}
+					?> Search!</button>
+				</div>
 				<input type="hidden" name="pg" value="0">
 				<input type="hidden" name="tp" value="0">
-				<br/>
-				<button type="submit">Search!</button>
 			</form>
 		</div>
 	</body>
